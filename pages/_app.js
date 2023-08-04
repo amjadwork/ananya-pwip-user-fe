8fe094e1bd2eeb04ad3bfe2c25109f523d21c9a8
skswain_pwip <sudhanshu@pwip.co>
2023-08-04 18:33:35 +0530

    WIP: costing edit screen meta and fixes

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,7 @@ function MyPWIPApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width; initial-scale=1; viewport-fit=cover"
         />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="white" />
         <meta name="theme-color" content="#005F81" />
       </Head>
       <Component {...pageProps} />
