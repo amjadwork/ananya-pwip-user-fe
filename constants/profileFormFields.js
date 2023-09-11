444efa522e33d4f394b49e8cea6ca3e30e923e9a
Ananya Singh <ananya@pwip.co>
2023-09-11 19:30:32 +0000

    CU-85zty1w5p_FEApp-Profile-UI

---
+++ constants/profileFormFields.js
@@ -0,0 +1,8 @@
+export const profileFormFields = [
+  { name: "fullName", label: "Name", type: "text" },
+  { name: "email", label: "Email", type: "email" },
+  { name: "number", label: "Number", type: "number" },
+  { name: "companyName", label: "Company Name", type: "text" },
+  { name: "profession", label: "Profession", type: "bottomsheet" },
+  { name: "gstNumber", label: "GST Number", type: "number" },
+];
