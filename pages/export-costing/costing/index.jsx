6aca86512cf93a38c11d547f7ba33494f5cec7bc
skswain_pwip <sudhanshu@pwip.co>
2023-11-23 16:01:41 +0530

    more UI header fix

--- pages/export-costing/costing/index.jsx
+++ pages/export-costing/costing/index.jsx
@@ -937,22 +937,22 @@ function CostingOverview() {
                         </span>
                       </div>
 
-                      <div className="inline-flex items-center justify-center space-x-3 text-pwip-v2-primary w-auto">
+                      <div className="inline-flex items-center justify-center space-x-3 text-pwip-v2-primary">
                         {lineBetweenLocation}
                       </div>
 
-                      <div className="inline-flex items-center justify-center space-x-3 w-auto">
+                      <div className="inline-flex items-center justify-center space-x-3">
                         <span className="text-pwip-v2-primary text-xs font-[700] font-sans line-clamp-1">
                           {generatedCostingData?.details?.sourceObject
                             ?.region || "-/-"}
                         </span>
                       </div>
 
-                      <div className="inline-flex items-center justify-center space-x-3 text-pwip-v2-primary w-auto">
+                      <div className="inline-flex items-center justify-center space-x-3 text-pwip-v2-primary">
                         {lineBetweenLocation}
                       </div>
 
-                      <div className="inline-flex items-center justify-center space-x-3 w-auto">
+                      <div className="inline-flex items-center justify-center space-x-3">
                         <span className="text-pwip-v2-primary text-xs font-[700] font-sans line-clamp-1">
                           {generatedCostingData?.details?.destinationObject
                             ?.portName || "-/-"}
