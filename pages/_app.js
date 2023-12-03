ff0b764eefbee3603efd6e4f17b0f8d956be356e
skswain_pwip <sudhanshu@pwip.co>
2023-12-03 17:21:05 +0530

    not to be full screen in standalone app - 2

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyPWIPApp({ Component, pageProps: { session, ...pageProps } }) {
             <Head>
               <meta
                 name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
               />
               <meta
                 name="apple-mobile-web-app-status-bar-style"
