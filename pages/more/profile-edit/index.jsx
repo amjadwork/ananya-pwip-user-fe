f40b34c46833edd6b666909b248120949cdc385b
Ananya Singh <ananya@pwip.co>
2023-12-20 16:27:02 +0530

    images etc etc

--- pages/more/profile-edit/index.jsx
+++ pages/more/profile-edit/index.jsx
@@ -11,11 +11,10 @@ import {
   pencilIcon,
   instagram,
   linkedin,
-  facebook,
   whatsapp,
   youtube,
   facebookIcon,
-  googleIcon,
+  profileBg,
 } from "../../../theme/icon";
 import { professionOptions } from "@/constants/professionOptions";
 import {
@@ -41,6 +40,8 @@ import {
   contactFields,
   personalFields,
   companyFields,
+  aboutFields,
+  aboutFieldsHeading,
   contactFieldsHeading,
   companyFieldsHeading,
   personalFieldsHeading,
@@ -193,14 +194,45 @@ function ProfileEdit() {
                 e.preventDefault();
                 handleSubmit();
               }}>
-              <div className="w-full h-24 p-4 text-[#003559]font-sans font-bold text-base text-left bg-[url('/assets/images/bg-profile.png')]  bg-cover">
+              <div className="w-full h-24 p-7 text-[#003559]font-sans font-bold text-xl text-left text-[#003559] bg-[url('/assets/images/bg-profile.png')]  bg-cover">
                 {fieldHeading.heading}
               </div>
               <div className="mx-7">
                 <div className="pb-7">
                   {fields.map((field, index) => (
                     <div className="relative">
-                      {
+                      {field.type === "textarea" ? (
+                        <div className="mb-2 mt-2">
+                          <label
+                            htmlFor={field.name}
+                            className="text-sm font-medium text-gray-900">
+                            {field.label}
+                          </label>
+                          <textarea
+                            id={field.name}
+                            name={field.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            maxLength={600}
+                            style={{
+                              textAlign: "left",
+                            }}
+                            className={`block w-full h-24 p-1 text-sm text-gray-900 rounded-md border ${
+                              errors[field.name] && touched[field.name]
+                                ? "border-red-300"
+                                : "border-[#006EB4]"
+                            } appearance-none focus:outline-none focus:ring-2 focus:border-pwip-primary peer`}
+                            placeholder={field.placeholder}
+                          />
+                          {errors[field.name] ? (
+                            <span
+                              className="absolute text-red-400 text-xs"
+                              style={{ top: "100%" }}>
+                              {errors[field.name]}
+                            </span>
+                          ) : null}
+                        </div>
+                      ) : (
                         <div className="mb-2 mt-2  overflow-x-auto">
                           <label
                             htmlFor={field.name}
@@ -231,9 +263,8 @@ function ProfileEdit() {
                             </span>
                           ) : null}
                         </div>
-                      }
+                      )}
                     </div>
-                    // <div>spectate</div>
                   ))}
                 </div>
 
@@ -349,132 +380,141 @@ function ProfileEdit() {
       <div className="w-full h-auto bg-white">
         <Header />
         <div className="w-full bg-white flex flex-col">
-          <div className="mt-12 h-48 pt-6 pl-4 bg-[url('/assets/images/bg-profile.png')] bg-cover">
+          <div className="mt-12 h-56 pt-14 pl-4 bg-[url('/assets/images/bg-profile.png')] bg-cover">
             <img
               className="w-[142px] h-[142px] rounded-full border-blue-950"
               src="https://via.placeholder.com/142x142"
             />
           </div>
-          <div className="w-full h-[92px] p-2 mb-5 bg-slate-100">
-            <div className=" text-sky-950 text-lg font-bold flex justify-between">
-              Anurag Mishra{" "}
-              <button
-                onClick={() => {
-                  handleFormFieldBottomSheet(
-                    personalFields,
-                    personalFieldsHeading
-                  );
-                }}>
-                {pencilIcon}
-              </button>
-            </div>
+          <div className="mx-2 mt-6">
+            <div className="w-full h-[92px] p-2 mb-5 bg-slate-100">
+              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+                Anurag Mishra{" "}
+                <button
+                  onClick={() => {
+                    handleFormFieldBottomSheet(
+                      personalFields,
+                      personalFieldsHeading
+                    );
+                  }}>
+                  {pencilIcon}
+                </button>
+              </div>
 
-            <div className=" text-neutral-700 text-xs font-normal leading-tight">
-              Bengaluru, Karnataka, India
+              <div className=" text-neutral-700 text-xs font-normal leading-tight">
+                Bengaluru, Karnataka, India
+              </div>
+              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
+                Managing Director, Mishra Exports
+              </div>
             </div>
-            <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-              Managing Director, Mishra Exports
+            <div className="w-full h-[92px] p-2 mb-5 bg-slate-100">
+              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+                Contact Details{" "}
+                <button
+                  onClick={() => {
+                    handleFormFieldBottomSheet(
+                      contactFields,
+                      contactFieldsHeading
+                    );
+                  }}>
+                  {pencilIcon}
+                </button>
+              </div>
+              <div className=" text-neutral-700 text-xs font-normal leading-tight">
+                mishra.anurag110@gmail.com
+              </div>
+              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
+                6371569477
+              </div>
             </div>
-          </div>
-          <div className="w-full h-[92px] p-2 mb-5 bg-slate-100">
-            <div className=" text-sky-950 text-lg font-bold flex justify-between">
-              Contact Details{" "}
-              <button
-                onClick={() => {
-                  handleFormFieldBottomSheet(
-                    contactFields,
-                    contactFieldsHeading
-                  );
-                }}>
-                {pencilIcon}
-              </button>
+            <div className="bg-white py-4 px-2">
+              <div className="w-full  text-gray-800 text-base font-bold flex justify-between">
+                <span>About</span>
+                <button
+                  onClick={() => {
+                    handleFormFieldBottomSheet(aboutFields, aboutFieldsHeading);
+                  }}>
+                  {pencilIcon}
+                </button>
+              </div>
+              <div className="w-full mt-2 text-sky-950 text-sm font-medium leading-snug">
+                Amar Singh is a second-generation rice miller. He inherited his
+                family's rice milling business, which has been operating for
+                over 30 years. Amar Singh has been managing the rice mill for
+                the past 20 years and has seen significant changes in the
+                industry during this time.
+              </div>
             </div>
-            <div className=" text-neutral-700 text-xs font-normal leading-tight">
-              mishra.anurag110@gmail.com
-            </div>
-            <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-              6371569477
-            </div>
-          </div>
-          <div className="bg-red-200 py-4 px-5">
-            <div className="w-[69px]  text-gray-800 text-base font-bold flex justify-between">
-              About
-              <button
-                onClick={() => {
-                  handlePersonalDetail();
-                }}>
-                {pencilIcon}
-              </button>
-            </div>
-            <div className="w-full mt-2 text-sky-950 text-sm font-medium leading-snug">
-              Amar Singh is a second-generation rice miller. He inherited his
-              family's rice milling business, which has been operating for over
-              30 years. Amar Singh has been managing the rice mill for the past
-              20 years and has seen significant changes in the industry during
-              this time.
-            </div>
-          </div>
-          <h2 className="mt-4 mb-5 text-pwip-v2-primary font-sans text-base font-bold">
-            I am a/an
-          </h2>
-          <div className="flex overflow-x-scroll hide-scroll-bar">
-            <div className="flex flex-nowrap">
-              {professionOptions.map((items, index) => {
-                return (
-                  <div
-                    key={items.label + (index + 1 * 2)}
-                    className="w-[116px] h-[116px] inline-block px-[15px] py-[18px] bg-blue-100 rounded-lg mr-4"
-                    style={{
-                      boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.12)",
-                      backdropFilter: "blur(8px)",
-                    }}>
-                    <div className="overflow-hidden w-[186px] h-auto inline-flex flex-col">
-                      <div className="mt-[10px] inline-flex items-center space-x-2 text-pwip-v2-primary-800 text-xs font-[600]">
-                        <span className="line-clamp-1">{items?.label}</span>
+            <h2 className="mt-4 mb-5 px-2 text-pwip-v2-primary font-sans text-base font-bold">
+              I am a/an
+            </h2>
+            <div className="flex overflow-x-scroll hide-scroll-bar px-2">
+              <div className="flex flex-between">
+                {professionOptions.map((items, index) => {
+                  return (
+                    <div>
+                      <div
+                        key={items.label + (index + 1 * 2)}
+                        className=" w-[116px] h-[116px] inline-block bg-blue-100 rounded-lg mr-4"
+                        style={{
+                          boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.12)",
+                          backdropFilter: "blur(8px)",
+                        }}>
+                        {/* <img
+                          className=""
+                          src={items.image}
+                        /> */}
+                        <div className="overflow-hidden w-[186px] h-auto inline-flex flex-col">
+                          <div className="mt-[10px] inline-flex items-center space-x-2 text-pwip-v2-primary-800 text-xs font-[600]"></div>
+                        </div>
+                      </div>
+                      <div className="mt-1 text-center text-sm font-semibold">
+                        {items?.label}
                       </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="w-full h-[92px] p-2 my-5 bg-slate-100">
-            <div className=" text-sky-950 text-lg font-bold flex justify-between">
-              Company
-              <button
-                onClick={() => {
-                  handleFormFieldBottomSheet(
-                    companyFields,
-                    companyFieldsHeading
                   );
-                }}>
-                {pencilIcon}
-              </button>
+                })}
+              </div>
             </div>
+            <div className="w-full h-[92px] p-2 my-5 bg-white">
+              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+                Company
+                <button
+                  onClick={() => {
+                    handleFormFieldBottomSheet(
+                      companyFields,
+                      companyFieldsHeading
+                    );
+                  }}>
+                  {pencilIcon}
+                </button>
+              </div>
 
-            <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-              Mishra Mills & Exports
+              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
+                Mishra Mills & Exports
+              </div>
+              <div className=" text-neutral-700 text-xs font-normal leading-tight">
+                Bangalore, Karnataka
+              </div>
+              <div className=" text-neutral-700 text-xs font-normal leading-tight">
+                GST: 83728934823468934
+              </div>
             </div>
-            <div className=" text-neutral-700 text-xs font-normal leading-tight">
-              Bangalore, Karnataka
-            </div>
-            <div className=" text-neutral-700 text-xs font-normal leading-tight">
-              GST: 83728934823468934
-            </div>
-          </div>
-          <div className="w-full h-[150px] p-2 mt-5 mb-10 bg-[url('/assets/images/bg-profile.png')] bg-cover">
-            <div className=" text-sky-950 text-lg font-bold mb-7">
-              Find me on
-            </div>
+            <div className="w-full h-[150px] p-2 bg-[url('/assets/images/bg-profile.png')] bg-cover">
+              <div className=" text-sky-950 text-lg font-bold mb-7">
+                Find me on
+              </div>
 
-            <div className="w-full flex flex-row justify-between">
-              {socialMediaIcons.map((icon, index) => (
-                <div
-                  key={index}
-                  className="w-[46px] h-[46px] bg-orange-50 rounded-lg p-3">
-                  {icon}
-                </div>
-              ))}
+              <div className="w-full flex flex-row justify-between">
+                {socialMediaIcons.map((icon, index) => (
+                  <div
+                    key={index}
+                    className="w-[46px] h-[46px] bg-orange-50 rounded-lg p-3">
+                    {icon}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
