d6bb5196fe1a524d78149407a9508d5f58937ca2
Ananya Singh <ananya@pwip.co>
2023-12-21 15:53:28 +0530

    default company icon, about field populating, css fixes

--- pages/more/profile-edit/index.jsx
+++ pages/more/profile-edit/index.jsx
@@ -6,6 +6,7 @@ import { useOverlayContext } from "@/context/OverlayContext";
 import ProfileDetailForm from "@/components/ProfileDetailForm";
 import {
   cameraIcon,
+  building,
   pencilIcon,
   instagram,
   linkedin,
@@ -59,6 +60,8 @@ function ProfileEdit() {
     youtube,
   ];
 
+  const { openBottomSheet } = useOverlayContext();
+
   useEffect(() => {
     if (token) {
       dispatch(fetchProfileRequest());
@@ -73,13 +76,6 @@ function ProfileEdit() {
     }
   }, [profileObject]);
 
-  const {
-    openBottomSheet,
-    closeBottomSheet,
-    openToastMessage,
-    // closeToastMessage,
-  } = useOverlayContext();
-
   const handleFormFieldBottomSheet = (fields, fieldHeading) => {
     setIsBottomSheetOpen(true);
     const content = (
@@ -90,7 +86,6 @@ function ProfileEdit() {
           fieldHeading={fieldHeading}
           userObject={userObject}
           profileObject={profileObject}
-          openBottomSheet={openBottomSheet}
         />
       </React.Fragment>
     );
@@ -119,8 +114,9 @@ function ProfileEdit() {
             />
           </div>
           <div className="mx-2 mt-6">
-            <div className="w-full h-[92px] p-2 mb-5 bg-[#F4FCFF]">
-              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+            {/* Personal Details Section*/}
+            <div className="w-full h-[92px] p-3 mb-6 bg-[#F4FCFF]">
+              <div className="text-[#003559] text-lg font-bold flex justify-between mb-2">
                 {userObject?.userData?.full_name}
                 <button
                   onClick={() => {
@@ -132,18 +128,23 @@ function ProfileEdit() {
                   {pencilIcon}
                 </button>
               </div>
-
-              <div className=" text-neutral-700 text-xs font-normal leading-tight">
-                {profileObject?.profileData?.city},{" "}
-                {profileObject?.profileData?.state},{" "}
+              <div className="text-[#263238] text-sm font-normal leading-tight">
+                {profileObject?.profileData?.headline ? (
+                  profileObject.profileData.headline
+                ) : (
+                  <div>Add Headline</div>
+                )}
+              </div>
+              <div className=" text-[#3B4241] text-xs font-normal leading-tight">
+                {profileObject?.profileData?.city},{""}
+                {profileObject?.profileData?.state},{""}
                 {profileObject?.profileData?.country}
               </div>
-              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-                {profileObject?.profileData?.headline}
-              </div>
             </div>
-            <div className="w-full h-[92px] p-2 mb-5 bg-[#F4FCFF]">
-              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+
+            {/* Contact Details Section*/}
+            <div className="w-full h-[92px] p-3 mb-6 bg-[#F4FCFF]">
+              <div className=" text-[#263238] text-base font-bold flex justify-between mb-2">
                 Contact Details{" "}
                 <button
                   onClick={() => {
@@ -155,15 +156,23 @@ function ProfileEdit() {
                   {pencilIcon}
                 </button>
               </div>
-              <div className=" text-neutral-700 text-xs font-normal leading-tight">
+              <div className=" text-[#3B4241] text-xs font-normal leading-tight">
                 {userObject?.userData?.email}
               </div>
-              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-                {userObject?.userData?.phone}
+              <div className="text-[#3B4241] text-xs font-normal leading-tight">
+                {userObject?.userData?.phone ? (
+                  userObject.userData.phone
+                ) : (
+                  <div>Add Phone Number</div>
+                )}
               </div>
             </div>
-            <div className="bg-white py-4 px-2">
-              <div className="w-full  text-gray-800 text-base font-bold flex justify-between">
+
+            {/* About Section*/}
+            <div className="bg-white p-3 mb-6">
+              <div
+                className="w-full  text-[#263238]
+              text-base font-bold flex justify-between mb-2">
                 <span>About</span>
                 <button
                   onClick={() => {
@@ -172,53 +181,65 @@ function ProfileEdit() {
                   {pencilIcon}
                 </button>
               </div>
-              <div className="w-full mt-2 text-sky-950 text-sm font-medium leading-snug">
-                {profileObject?.profileData?.about}
+              <div
+                className="w-full text-[#003559]
+              text-sm font-medium leading-snug">
+                {profileObject?.profileData?.bio ? (
+                  profileObject.profileData.bio
+                ) : (
+                  <span>Tell us more about yourself.</span>
+                )}
               </div>
             </div>
-            <h2 className="mt-4 mb-5 px-2 text-pwip-v2-primary font-sans text-base font-bold">
-              I am a/an
-            </h2>
-            <div className="flex overflow-x-scroll hide-scroll-bar px-2">
-              <div className="flex flex-between">
-                {professionOptions.map((items, index) => {
-                  const profession = profileObject?.profileData?.profession;
-                  return (
-                    <div>
-                      <div
-                        key={items.label + (index + 1 * 2)}
-                        className={`w-[116px] h-[116px] inline-block bg-[#C9EEFF] rounded-lg mr-4 ${
-                          isProfessionSelected && profession === items.value
-                            ? "opacity-100"
-                            : "opacity-25"
-                        }`}
-                        style={{
-                          boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.12)",
-                          backdropFilter: "blur(8px)",
-                        }}>
-                        <img
-                          className="h-full w-full object-contain"
-                          src={items.image}
-                        />
-                        <div className="overflow-hidden w-[186px] h-auto inline-flex flex-col">
-                          <div className="mt-[10px] inline-flex items-center space-x-2 text-pwip-v2-primary-800 text-xs font-[600]"></div>
+
+            {/* Profession Section*/}
+            <div className="px-3 mb-6">
+              <div className="mb-3.5 text-[#263238] font-sans text-base font-bold">
+                I am a/an
+              </div>
+              <div className="flex overflow-x-scroll hide-scroll-bar">
+                <div className="flex flex-between">
+                  {professionOptions.map((items, index) => {
+                    const profession = profileObject?.profileData?.profession;
+                    return (
+                      <div>
+                        <div
+                          key={items.label + (index + 1 * 2)}
+                          className={`w-[116px] h-[116px] inline-block bg-[#C9EEFF] rounded-lg mr-5 ${
+                            isProfessionSelected && profession === items.value
+                              ? "opacity-100"
+                              : "opacity-25"
+                          }`}
+                          style={{
+                            boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.12)",
+                            backdropFilter: "blur(8px)",
+                          }}>
+                          <img
+                            className="h-full w-full object-contain"
+                            src={items.image}
+                          />
+                          <div className="overflow-hidden h-auto flex-col">
+                            <div
+                              className={`mt-2 text-center text-black text-xs font-semibold ${
+                                isProfessionSelected &&
+                                profession === items.value
+                                  ? "text-pwip-v2-primary"
+                                  : "text-gray-400"
+                              }`}>
+                              {items?.label}
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div
-                        className={`mt-1 text-center text-sm font-semibold ${
-                          isProfessionSelected && profession === items.value
-                            ? "text-pwip-v2-primary"
-                            : "text-gray-400"
-                        }`}>
-                        {items?.label}
-                      </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            <div className="w-full h-[92px] p-2 my-5 bg-white">
-              <div className=" text-sky-950 text-lg font-bold flex justify-between">
+
+            {/* Company Details Section*/}
+            <div className="w-full h-[100px] p-3 mb-6 bg-white">
+              <div className=" text-[#263238] text-base font-bold flex justify-between mb-2">
                 Company Details
                 <button
                   onClick={() => {
@@ -230,15 +251,34 @@ function ProfileEdit() {
                   {pencilIcon}
                 </button>
               </div>
+              <div className="flex">
+                <div className="text-gray-800 text-sm font-normal leading-tight mb-2">
+                  {building}
+                </div>
 
-              <div className="w-[231px] text-gray-800 text-sm font-normal leading-tight">
-                {profileObject?.profileData?.companyName}
-              </div>
-              <div className=" text-neutral-700 text-xs font-normal leading-tight">
-                {profileObject?.profileData?.companyAddress}
-              </div>
-              <div className=" text-neutral-700 text-xs font-normal leading-tight">
-                GST: {profileObject?.profileData?.gstin}
+                <div className="ml-3">
+                  <div className="text-gray-800 text-sm font-normal leading-tight mb-1.5">
+                    {profileObject?.profileData?.companyName ? (
+                      profileObject.profileData.companyName
+                    ) : (
+                      <div>Add Company Name</div>
+                    )}
+                  </div>
+                  <div className="text-neutral-700 text-xs font-normal leading-tight mb-px">
+                    {profileObject?.profileData?.companyAddress ? (
+                      profileObject.profileData.companyAddress
+                    ) : (
+                      <div>Add Company Address</div>
+                    )}
+                  </div>
+                  <div className="text-neutral-700 text-xs font-normal leading-tight">
+                    {profileObject?.profileData?.gstin ? (
+                      profileObject.profileData.gstin
+                    ) : (
+                      <div>Add GST Number</div>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
             <div className="w-full h-[150px] p-2 bg-[url('/assets/images/bg-profile.png')] bg-cover bg-opacity-40">
