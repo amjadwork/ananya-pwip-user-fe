45308f56c6ae6b46c43957f1e53399048c98ca93
Ananya Singh <ananya@pwip.co>
2024-02-27 14:58:11 +0530

    sub detail component deleted and moved in the pages

--- pages/subscription-details/index.jsx
+++ pages/subscription-details/index.jsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 
 import withAuth from "@/hoc/withAuth";
 import AppLayout from "@/layouts/appLayout.jsx";
-import SubscriptionDetailCard from "@/components/SubscriptionDetailCard";
+import { inSubscription } from "../../theme/icon";
 
 // Import Components
 import { Header } from "@/components/Header";
@@ -38,7 +38,95 @@ function SubscriptionDetails() {
             className={`min-h-[calc(100vh-120px)] inline-flex  bg-pwip-v2-primary-100 flex-col h-full w-full px-5 pt-[82px] pb-[120px] overflow-auto hide-scroll-bar`}
           >
             <React.Fragment>
-              <SubscriptionDetailCard />
+              <div className="w-full h-auto bg-white p-4 border border-solid border-[#ccc] shadow rounded-xl">
+                <div className="flex">
+                  {inSubscription}
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-sm font-semibold ml-2 flex items-center">
+                      Rice Price
+                    </div>
+                    <div className="text-xs h-5 w-14 text-white bg-[#25AF7D] px-3 flex items-center rounded-md ">
+                      Active
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full flex justify-between text-sm mt-3">
+                  <div className="font-semibold">Basic Plan</div>
+                  <div className="font-semibold">
+                    &#8377;299<span className="font-normal">/mo</span>
+                  </div>
+                </div>
+                <div className=" w-full flex text-xs mt-1">
+                  <div className="text-[#808080]">Validity: 30 days</div>{" "}
+                  <div className="text-[#B5B5B5]">
+                    (27 April - 27 May, 2024)
+                  </div>
+                </div>
+                <hr className="w-full mt-3"></hr>
+                <div className="flex flex-col">
+                  <div className=" w-full h-10 border-b border-solid border-[#F9F9F9] flex items-center ">
+                    <div className="flex justify-left">
+                      <img
+                        src="/assets/images/paper-bill.png"
+                        alt="paper-bill"
+                        width={14}
+                        height={14}
+                      />
+                      <h2 className="text-[#1B1B1B] font-normal text-xs ml-2">
+                        Order #846748294
+                      </h2>
+                    </div>
+                  </div>
+                  <div className=" w-full h-10 border-b border-solid border-[#F9F9F9] flex items-center  ">
+                    <div className="flex justify-left">
+                      <img
+                        src="/assets/images/paper-bill.png"
+                        alt="paper-bill"
+                        width={14}
+                        height={14}
+                      />
+                      <h2 className="text-[#1B1B1B] font-normal text-xs ml-2">
+                        Transaction ID #T8467482
+                      </h2>
+                    </div>
+                  </div>
+                  <div className=" w-full h-10 border-b border-solid border-[#F9F9F9] flex items-center  ">
+                    <div className="flex justify-left">
+                      <img
+                        src="/assets/images/paper-bill.png"
+                        alt="paper-bill"
+                        width={14}
+                        height={14}
+                      />
+                      <h2 className="text-[#1B1B1B] font-normal text-xs ml-2">
+                        Payment made on 27 April, 2024 at 07:49 PM
+                      </h2>
+                    </div>
+                  </div>
+                  <div className=" w-full h-10 flex items-center ">
+                    <div className="flex justify-left">
+                      <img
+                        src="/assets/images/paper-bill.png"
+                        alt="paper-bill"
+                        width={14}
+                        height={14}
+                      />
+                      <h2 className="text-[#1B1B1B] font-normal text-xs ml-2">
+                        Request subscription cancellation
+                      </h2>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => {
+                      //onclick function
+                    }}
+                    className="border border-[#2072AB] w-[40%] mt-4 p-1 rounded-md text-pwip-primary text-center text-xs"
+                  >
+                    Download Invoice
+                  </button>
+                </div>
+              </div>
             </React.Fragment>
           </div>
         </div>
