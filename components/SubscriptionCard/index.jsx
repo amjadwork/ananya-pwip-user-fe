7e5d23262b33493f66e90e6096e295384bdce8f9
Ananya Singh <ananya@pwip.co>
2024-04-12 19:29:23 +0000

    Cu 86cutp2k8 fe app subscription api integration to get

--- components/SubscriptionCard/index.jsx
+++ components/SubscriptionCard/index.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import { useRouter } from "next/router";
 import { inSubscription } from "../../theme/icon";
@@ -6,18 +8,30 @@ const SubscriptionCard = ({
   subscriptionName,
   subscriptionType,
   subscriptionValidity,
+  subscription_id,
 }) => {
   const router = useRouter();
   const isPremium = subscriptionType === "Premium";
 
+  const handleViewDetails = () => {
+    router.push({
+      pathname: "/subscription-details",
+      query: {
+        subscriptionName,
+        subscriptionType,
+        subscription_id,
+      },
+    });
+  };
+
   return (
-    <div className="w-full p-4 border-b border-solid border-[#ccc]">
+    <div className="w-full h-16 p-4 border-b border-solid border-[#ccc] mt-4">
       <div className="flex item-center">
         {inSubscription}
         <div className="w-full">
           <div className="flex justify-between">
-            <h2 className="text-sm font-semibold ml-2">{subscriptionName}</h2>
-            <p className="text-[rgb(27,27,27)] text-xs bg-[#F6F6F6] px-3 pt-0.5 rounded-md mb-0.5 flex">
+            <h2 className="text-xs font-semibold ml-3">{subscriptionName}</h2>
+            <p className="text-[rgb(27,27,27)] text-[10px] bg-[#F6F6F6] px-3 pt-0.5 rounded-md mb-0.5 flex">
               {isPremium && (
                 <div className=" flex items-center mr-0.5">
                   <img
@@ -31,13 +45,16 @@ const SubscriptionCard = ({
               {subscriptionType}
             </p>
           </div>
-          <div className="flex justify-between">
-            <h2 className="text-[#808080] font-normal text-xs ml-2">
+          <div className="flex justify-between mt-1">
+            <h2 className="text-[#808080] font-normal text-[10px] ml-3 ">
               {subscriptionValidity}
             </h2>
-            <p onClick={() => {
-                router.push("/subscription-details");
-              }}className="text-[#2072AB] text-xs">View details</p>
+            <p
+              onClick={handleViewDetails}
+              className="text-[#2072AB] text-[10px]"
+            >
+              View details
+            </p>
           </div>
         </div>
       </div>
